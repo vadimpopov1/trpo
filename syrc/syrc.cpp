be38ec4c0b18913764866361ad5d6aec76f1e6cc
vadimpopov1 <bonia07052006@icloud.com>
2026-03-17 18:26:19 +0700

    refactoring

--- syrc/syrc.cpp
+++ syrc/syrc.cpp
@@ -3,92 +3,74 @@
 #include <chrono>
 #include <random>
 #include <dlfcn.h>
-#include <thread>
-#include <mutex>
-#include <functional>
 
 using namespace std;
 using namespace std::chrono;
 
 typedef void (*ssyrk_func)(int, int, int, int, int, float, float*, int, float, float*, int);
 
-void ssyrk_simple(char uplo, char trans, int n, int k, float alpha, float* A, int lda, float beta, float* C, int ldc, int num_threads) {
+void ssyrk_simple(char uplo, char trans, int n, int k, float alpha, float* A, int lda, float beta, float* C, int ldc) {
     if (n <= 0 || k < 0) return;
     
     bool upper = (uplo == 'U' || uplo == 'u');
     bool normal = (trans == 'N' || trans == 'n');
     
-    auto compute_range = [&](int start_j, int end_j) {
-        if (normal) {
-            if (upper) {
-                for (int j = start_j; j < end_j; j++) {
-                    for (int i = 0; i <= j; i++) {
-                        float sum = 0;
-                        for (int l = 0; l < k; l++) {
-                            sum += A[i + l*lda] * A[j + l*lda];
-                        }
-                        if (beta == 0)
-                            C[i + j*ldc] = alpha * sum;
-                        else
-                            C[i + j*ldc] = alpha * sum + beta * C[i + j*ldc];
+    if (normal) {
+        if (upper) {
+            for (int j = 0; j < n; j++) {
+                for (int i = 0; i <= j; i++) {
+                    float sum = 0;
+                    for (int l = 0; l < k; l++) {
+                        sum += A[i + l*lda] * A[j + l*lda];
                     }
-                }
-            } else {
-                for (int j = start_j; j < end_j; j++) {
-                    for (int i = j; i < n; i++) {
-                        float sum = 0;
-                        for (int l = 0; l < k; l++) {
-                            sum += A[i + l*lda] * A[j + l*lda];
-                        }
-                        if (beta == 0)
-                            C[i + j*ldc] = alpha * sum;
-                        else
-                            C[i + j*ldc] = alpha * sum + beta * C[i + j*ldc];
-                    }
+                    if (beta == 0)
+                        C[i + j*ldc] = alpha * sum;
+                    else
+                        C[i + j*ldc] = alpha * sum + beta * C[i + j*ldc];
                 }
             }
         } else {
-            if (upper) {
-                for (int j = start_j; j < end_j; j++) {
-                    for (int i = 0; i <= j; i++) {
-                        float sum = 0;
-                        for (int l = 0; l < k; l++) {
-                            sum += A[l + i*lda] * A[l + j*lda];
-                        }
-                        if (beta == 0)
-                            C[i + j*ldc] = alpha * sum;
-                        else
-                            C[i + j*ldc] = alpha * sum + beta * C[i + j*ldc];
+            for (int j = 0; j < n; j++) {
+                for (int i = j; i < n; i++) {
+                    float sum = 0;
+                    for (int l = 0; l < k; l++) {
+                        sum += A[i + l*lda] * A[j + l*lda];
                     }
-                }
-            } else {
-                for (int j = start_j; j < end_j; j++) {
-                    for (int i = j; i < n; i++) {
-                        float sum = 0;
-                        for (int l = 0; l < k; l++) {
-                            sum += A[l + i*lda] * A[l + j*lda];
-                        }
-                        if (beta == 0)
-                            C[i + j*ldc] = alpha * sum;
-                        else
-                            C[i + j*ldc] = alpha * sum + beta * C[i + j*ldc];
-                    }
+                    if (beta == 0)
+                        C[i + j*ldc] = alpha * sum;
+                    else
+                        C[i + j*ldc] = alpha * sum + beta * C[i + j*ldc];
                 }
             }
         }
-    };
-    
-    vector<thread> threads;
-    int chunk_size = n / num_threads;
-    
-    for (int t = 0; t < num_threads; t++) {
-        int start_j = t * chunk_size;
-        int end_j = (t == num_threads - 1) ? n : (t + 1) * chunk_size;
-        threads.emplace_back(compute_range, start_j, end_j);
-    }
-    
-    for (auto& th : threads) {
-        th.join();
+    } else {
+        if (upper) {
+            for (int j = 0; j < n; j++) {
+                for (int i = 0; i <= j; i++) {
+                    float sum = 0;
+                    for (int l = 0; l < k; l++) {
+                        sum += A[l + i*lda] * A[l + j*lda];
+                    }
+                    if (beta == 0)
+                        C[i + j*ldc] = alpha * sum;
+                    else
+                        C[i + j*ldc] = alpha * sum + beta * C[i + j*ldc];
+                }
+            }
+        } else {
+            for (int j = 0; j < n; j++) {
+                for (int i = j; i < n; i++) {
+                    float sum = 0;
+                    for (int l = 0; l < k; l++) {
+                        sum += A[l + i*lda] * A[l + j*lda];
+                    }
+                    if (beta == 0)
+                        C[i + j*ldc] = alpha * sum;
+                    else
+                        C[i + j*ldc] = alpha * sum + beta * C[i + j*ldc];
+                }
+            }
+        }
     }
 }
 
@@ -96,7 +78,7 @@ int main(int argc, char* argv[]) {
     int n = 4096;
     int k = 2048;
     
-    int num_threads = thread::hardware_concurrency();
+    int num_threads = 1;
     
     if (argc > 1) {
         num_threads = atoi(argv[1]);
@@ -123,7 +105,7 @@ int main(int argc, char* argv[]) {
     }
     
     auto start = high_resolution_clock::now();
-    ssyrk_simple('U', 'N', n, k, 1.0f, A.data(), lda, 0.5f, C1.data(), ldc, num_threads);
+    ssyrk_simple('U', 'N', n, k, 1.0f, A.data(), lda, 0.5f, C1.data(), ldc);
     auto end = high_resolution_clock::now();
     double simple_time = duration_cast<microseconds>(end - start).count() / 1000000.0;
     cout << "Простая реализация: " << simple_time << " сек" << endl;
